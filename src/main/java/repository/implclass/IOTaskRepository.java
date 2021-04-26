99a5bc22fb9857d4138edc961bb6090311cbb102
Ovani4 <ovani4@ya.ru>
2021-04-27 00:02:05 +0300

    create IO class

---
+++ src/main/java/repository/implclass/IOTaskRepository.java
@@ -0,0 +1,32 @@
+package repository.implclass;
+
+import repository.TaskRepository;
+
+import java.util.List;
+
+public class IOTaskRepository implements TaskRepository {
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object deleteById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object o) {
+        return null;
+    }
+}
---
+++ src/main/java/repository/implclass/IOUserRepository.java
@@ -0,0 +1,32 @@
+package repository.implclass;
+
+import repository.UserRepository;
+
+import java.util.List;
+
+public class IOUserRepository implements UserRepository {
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object deleteById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object o) {
+        return null;
+    }
+}
